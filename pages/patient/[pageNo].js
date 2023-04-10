203bac90cbbe4c8a4ed44e41ab78edae3cb5127b
Utkarsh Mishra <UtkarshM@emids.com>
2023-04-10 17:13:07 +0530

    Added dynamic route with Apollo Client/GraphQl

--- pages/patient/[pageNo].js
+++ pages/patient/[pageNo].js
@@ -1,13 +1,24 @@
 import { useEffect } from "react";
+import { gql } from "@apollo/client";
+import client from "../../apolloClient";
+import pino from "../../logger";
+import Head from "next/head";
 
 export const getStaticPaths = async () => {
   try {
-    const res = await fetch("https://dummyjson.com/posts");
-    const { posts } = await res.json();
+    const { data } = await client.query({
+      query: gql`
+        query Countries {
+          countries {
+            code
+          }
+        }
+      `,
+    });
 
-    const paths = posts.map((curElem) => {
+    const paths = data.countries.map((curElem) => {
       return {
-        params: { pageNo: curElem.id.toString() },
+        params: { pageNo: curElem.code.toString() },
       };
     });
 
@@ -23,12 +34,34 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
   try {
     const id = context.params.pageNo;
-    const res = await fetch(`https://dummyjson.com/posts/${id}`);
-    const data = await res.json();
+    pino.info(`id ${id}`);
+    const { data } = await client.query({
+      query: gql`
+        query Country($code: ID!) {
+          country(code: $code) {
+            code
+            name
+            currency
+            capital
+            native
+            emoji
+            languages {
+              code
+              name
+            }
+          }
+        }
+      `,
+      variables: {
+        code: id,
+      },
+    });
+
+    pino.info(`Patient details response: ${JSON.stringify(data)}`);
 
     return {
       props: {
-        data,
+        data: data.country,
       },
     };
   } catch (error) {
@@ -41,14 +74,20 @@ const PatientData = ({ data, setView }) => {
     setView(true);
   }, []);
 
-  const { id, title, body } = data;
+  const { name, native, capital, languages, currency, emoji } = data;
   return (
     <>
-      <div className="container">
-        <div key={id} className="cards_details">
-          <h3 className="id_style">{id}</h3>
-          <h1 className="effect_title">{title}</h1>
-          <h3 className="para">{body}</h3>
+      <div className="cards_details">
+        <Head>
+          <title>Patient Country Details</title>
+        </Head>
+        <div className="container" key={languages[0].code}>
+          <h2 className="id_style">Patient Country : {name}</h2>
+          <h3 className="val_style">Patient Language : {languages[0].name}</h3>
+          <h3 className="val_style">Native : {native}</h3>
+          <h3 className="val_style">Capital : {capital}</h3>
+          <h3 className="val_style">Currency : {currency}</h3>
+          <h3 className="val_style">Emoji : {emoji}</h3>
         </div>
       </div>
     </>
