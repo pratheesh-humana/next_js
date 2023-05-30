bdbdc53ce5da62ec88093d4ba4297326ed27fb7e
Pushpa Sharma <PushpaS@emids.com>
2023-05-30 18:39:50 +0530

    Prescriber Changes new

--- components/Prescriber.js
+++ components/Prescriber.js
@@ -4,87 +4,27 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { useState } from "react";
-import { useQuery, gql } from "@apollo/client";
-import pino from "../logger";
-
-const PRESCRIBER_QUERY = gql`
-  query ExampleQuery($physicianNpi: String!) {
-    introduction
-    patients(physicianNpi: $physicianNpi) {
-      patients {
-        physician {
-          firstName
-          lastName
-        }
-      }
-    }
-  }
-`;
-
 const Prescriber = () => {
-  const { data, error, loading } = useQuery(PRESCRIBER_QUERY, {
-    variables: { physicianNpi: "12343212" },
-  });
-
   const [view, setView] = useState("1");
-
-  if (loading) {
-    return <p>Loading ...</p>;
-  }
-
-  console.log(data);
-  pino.info(`Data: ${data}`);
-
-  if (error) {
-    pino.error(`Error ${error}`);
-
-    return null;
-  }
-
   const handleChange = (event) => {
     setView(event.target.value);
   };
-
   return (
-    <>
-      <div>
-        <select class="form-select" aria-label="Default select example">
-          {data?.patients?.patients?.map((res) => {
-            const { firstName } = res.physician;
-            console.log("11122", firstName);
-            return (
-              <div>
-                <option value="1">{firstName}</option>
-              </div>
-            );
-          })}
-        </select>
-      </div>
-      <Box sx={{ minWidth: 300 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Prescriber</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={view}
-            label="Age"
-            onChange={handleChange}
-          >
-            {data?.patients?.patients?.map((res) => {
-              const { firstName, lastName } = res.physician;
-              return (
-                <div>
-                  <MenuItem key={lastName} value="1">
-                    {firstName}
-                  </MenuItem>
-                </div>
-              );
-            })}
-          </Select>
-        </FormControl>
-      </Box>
-    </>
+    <Box sx={{ minWidth: 300 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Prescriber</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={view}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value="1">Eric Richards NP-NPI#7234223044</MenuItem>
+          <MenuItem value="2">John Doe NP -NPI#8745824088</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
   );
 };
-
 export default Prescriber;
